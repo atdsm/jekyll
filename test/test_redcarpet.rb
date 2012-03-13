6903eb2527131e2843d664cbc0b2cd745d455852
Steve Frank <steve.m.frank@gmail.com>
2012-03-13 14:40:52 -0600

    Manually added redcarpet 2.1 support as per mojombo:jekyll pull request 450

--- test/test_redcarpet.rb
+++ test/test_redcarpet.rb
@@ -4,18 +4,36 @@ class TestRedcarpet < Test::Unit::TestCase
   context "redcarpet" do
     setup do
       config = {
-        'redcarpet' => { 'extensions' => ['smart'] },
+	    ## MANUALLY PATCHED FROM https://github.com/mojombo/jekyll/pull/450/
+        'redcarpet' => {
+          'extensions' => ['strikethrough', 'smart'],
+          'render_options' => ['filter_html']
+        },
+		## END MANUAL PATCH
         'markdown' => 'redcarpet'
       }
       @markdown = MarkdownConverter.new config
     end
-
+    
     should "pass redcarpet options" do
       assert_equal "<h1>Some Header</h1>", @markdown.convert('# Some Header #').strip
     end
     
-    should "pass redcarpet extensions" do
+    ## MANUALLY PATCHED FROM https://github.com/mojombo/jekyll/pull/450/
+	should "pass redcarpet SmartyPants option" do
+	## END MANUAL PATCH
       assert_equal "<p>&ldquo;smart&rdquo;</p>", @markdown.convert('"smart"').strip
     end
+	
+    ## MANUALLY PATCHED FROM https://github.com/mojombo/jekyll/pull/450/
+    should "pass redcarpet extensions" do
+      assert_equal "<p><del>deleted</del></p>", @markdown.convert('~~deleted~~').strip
+    end
+
+    should "pass redcarpet render options" do
+      assert_equal "<p><strong>bad code not here</strong>: i am bad</p>", @markdown.convert('**bad code not here**: <script>i am bad</script>').strip
+    end
+	## END MANUAL PATCH
+	
   end
 end
